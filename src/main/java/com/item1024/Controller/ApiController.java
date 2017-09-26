fd409d8b8cb09fcaa7b44ea9c866530777db2f3a
limingyang <1095001875@qq.com>
2017-09-26 14:41:47 +0800

    wdful

---
+++ src/main/java/com/item1024/Controller/ApiController.java
@@ -0,0 +1,11 @@
+package com.item1024.Controller;
+
+/**
+ * @author 阳十三
+ * @email wdful165177@gmail.com
+ * @blog http://www.item1024.com
+ * @date 2017/9/26
+ */
+
+public class ApiController {
+}
